37b91ed59eca97f4730723ccfb08f99787f2d848
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2023-09-14 23:42:58 -0700

    Add files via upload

---
+++ Blog/vars.js
@@ -0,0 +1,242 @@
+pre = `
+
+
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="icon" type="image/x-icon" href="ico.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    @font-face {
+  font-family: 'Open';
+  src: url('os.otf') format('opentype');   
+}
+@font-face {
+  font-family: 'Openr';
+  src: url('osr.otf') format('opentype');   
+}
+@import url('https://fonts.googleapis.com/css2?family=Rajdhani&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Rajdhani&display=swap');
+</style>
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="sw.css">
+<link rel="stylesheet" href="this.css">
+<link rel="stylesheet" href="search.css">
+<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="anim.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <meta charset="utf-8">
+
+  <script src="index.js"></script>
+<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+<!-- <script src="grid.js"></script> -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+</head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body  style="background-color: #212121" >
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+  
+
+  
+  <div id="log" class="p-2" style="height: 50px; ">
+    <br>
+    <h3  class=" float-start" style="color: white;padding-left: 2.5%;font-family: 'Open', cursive;">Anim Alley</h3>
+    
+    <p class="float-right pop"  style="margin-top: 10px; color: white; padding-right: 2.5%;font-family: 'Rajdhani', sans-serif; "><a href="search.html"><span class="material-symbols-outlined">
+      ambient_screen
+      </span></p></a>
+      <p class="float-right pop"  style="margin-top: 10px; color: white; padding-right: 2.5%;font-family: 'Rajdhani', sans-serif; "><a href="searchit.html"><span class="material-symbols-outlined">
+        search
+        </span></p></a>
+        <p class="float-right pop" style="margin-top: 10px; color: white; padding-right: 2.5%;font-family: 'Rajdhani', sans-serif; "><a href="index.html"><span class="material-symbols-outlined">
+            home
+          </span></p></a>
+    
+  </div>
+  <center>
+  <div id="logs"  class="p-2">
+
+  <img  src="https://animealley.online/logo%20svg.png" class="" style=" height: 25%; width: 23%; margin-left: 5%; padding-top: 1%;" alt="">
+    <p class="float-right pop"  style="margin-top: 35px; color: white; padding-right: 2.5%;font-family: 'Rajdhani', sans-serif; "><a href="search.html"><span class="material-symbols-outlined">
+      account_circle
+      </span></p></a>
+    
+  </div>
+  <div class="line"></div>
+  <div id="xpp" style="padding-top: 55px; "></div>
+</center>
+
+
+
+ 
+
+ 
+<div class="row" >
+  <div class="col-sm"  style="height: 330px;  padding-left: 2%; " id="ma" > `
+     
+
+
+
+
+    
+    
+    
+prepre = `  
+<br>
+  
+  <center>
+    
+   
+<div id="rel" class="row align-items-center justify-content-center" style="margin-top: 20px; background-color: #2A2A2A;width: 96%; height: fit-content; border-color:#707070;border-style: solid;border-width: thin;"> 
+ <h2 style="color: #F3CE13; font-family: 'Open'; margin-top: 3px; margin-bottom: 5px; font-size: 25px; margin-top: 10px; margin-bottom: 10px;">Related: Top 30+ Unforgettable Trap Characters in Anime </h2>
+</div>
+
+<div id="opk" class="row"><h3 class="xp">September 10, 2023</h3>
+  <h3 class="xp">Written By: Jack</h3>
+
+  <a href="news.html"><div style="background-color:rgb(218, 23, 23); border-radius: 30px; width: 85%; margin-top: 10px;"><p class="xp">Anime News</p></div></a>
+<div class="line"></div>
+
+<br>
+</div>
+<div id="opp">
+  <br>
+  <div class="line"></div>
+  
+  <br>
+  </div>
+
+`
+post = 
+
+`
+<div class="hr"></div>
+<h3 style="font-family: 'Open'; color: white;">Related: Top 30+ Unforgettable Trap Characters in Anime</h3>
+<div class="hr"></div>
+<br>
+<div class="row" style="width: 60%;">
+  <div class="col-sm pop"><a id="" href="">
+   <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1667/135587.jpg'); width: 220px; height: 120px;"></div>
+    <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+  </div>
+  <div class="col-sm pop"><a id="" href="">
+    <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1446/131578.webp'); width: 220px; height: 120px;"></div>
+    <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+  </div>
+  <div class="col-sm pop"><a id="" href="">
+    <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1984/136274.webp'); width: 220px; height: 120px;"></div>
+    <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+  </div>
+  <div class="col-sm pop"><a id="" href="">
+    <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1147/122444.jpg'); width: 220px; height: 120px;"></div>
+    <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+  </div>
+  </div>
+
+
+<div class="hr"></div>
+<div class="row" id="prpx">
+
+<div class="col-lg pop" style="margin-top: 80px;padding-right: 70px;">
+    <a href=""><h3  align="right" style="color: white; font-family: 'Open';">PREVIOUS <br>
+      Final Chapter of Hyaku Jū Gattai by Yorudon</h3></a>
+    
+</div>
+  <div id="vrx" class="col-md-auto" style="margin-left: 3%;"><div class="vr" style="color: white; height: 70%;margin-top: 50px;"></div></div>
+  <div id="hrx" class="col-md-auto"><div class="hr" style="color: white; width: 80%; margin-top: 50px;"></div></div>
+<div class="col-lg pop" style="margin-top: 80px; padding-left: 70px;">
+  <a href=""><h3 align="left" style="color: white; font-family: 'Open';">NEXT<br>
+    Manga Debut for Bullbuster Project</h3></a>
+  
+ </div>
+  
+ </div>
+ 
+ 
+ <div class="hr" style="margin-top: 80px;"></div>
+ <br>
+ <div style="width: 60%;"><h4 align="left" style="color: white; font-family: 'Open'; margin-left: 3%;">You May Also Like</h4></div>
+ <br>
+ <br>
+ <div class="row"  style="width: 60%;">
+  <div class="col-sm pop"><a id="" href="">
+    <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1667/135587.jpg'); width: 220px; height: 120px;"></div>
+     <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+   </div>
+   <div class="col-sm pop"><a id="" href="">
+     <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1446/131578.webp'); width: 220px; height: 120px;"></div>
+     <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+   </div>
+   <div class="col-sm pop"><a id="" href="">
+     <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1984/136274.webp'); width: 220px; height: 120px;"></div>
+     <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+   </div>
+   <div class="col-sm pop"><a id="" href="">
+     <div style="background-image: url('https://cdn.myanimelist.net/images/anime/1147/122444.jpg'); width: 220px; height: 120px;"></div>
+     <p class="xp" style="padding-top: 7px;">Unforgettable Trap Characters in Anime</p></a>
+   </div>
+
+  </div>
+ 
+
+  <div class="hr"></div>
+  <br>
+  
+  <a href=""><h4 class="pop"  onclick="top()" style="font-family: 'Openr' ; color: #5e5e5e; font-size: 18px;">BACK TO TOP   </h4></a>
+  <br>
+
+  <div class="row"  id="crx" style="padding-top: 50px;">
+    
+    <img class="pop" src="logo svg.png" style="height: 95px; width: 130px;" onclick="opr(io)" alt="">
+    <p class="xp float-lefts" style="width: 500px; padding-top: 35px; color: #5e5e5e; font-size: 16px; ">Copyright © AnimeAlley All Rights Reserved</p>
+
+<p  style="font-family: 'Openr' ; color: #5e5e5e; font-size: 14px; padding-left: 30px;"  >Made with ❤ for anime Lovers</p>
+
+</div>
+</div>
+<br>
+<div class="row" id="mob" style="padding-top: 25px;">
+  <img class="pop" src="logo svg.png" style="height: 75px; width: 100px;  padding-bottom: 2px;" onclick="opr(io)" alt="">
+    <p class="xp float-lefts" style="width: 500px; padding-top: 35px; color: #5e5e5e; font-size: 16px; ">Copyright © AnimeAlley All Rights Reserved</p>
+
+<p  style="font-family: 'Openr' ; color: #5e5e5e; font-size: 14px; padding-left: 30px;">Made with ❤ for anime Lovers By Jack</p>
+  </div>
+
+</center>
+        
+    
+  <center>
+  
+
+
+
+
+</center>
+</body>
+
+
+<script>
+
+
+
+</script>
+
+
+</html>
+
+
+
+`
